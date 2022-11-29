286c4acdd5bb44275ac1641c5a2f525b7e2964ea
Óskar Bjarnason <oskarg22@ru.is>
2022-11-29 14:23:53 +0000

    byrjun a ui

---
+++ RU Basement Open/ui/main_ui.py
@@ -0,0 +1,33 @@
+class Main_UI:
+    def __init__(self):
+        print("inside main_UI")
+
+    def menu_output(self):
+        '''starting menu'''
+        print("********************************************")
+        print("*                   Hello!                 *")
+        print("*                                          *")
+        print("*             (1)   admin?                 *")
+        print("*             (2) captain?                 *")
+        print("*             (3)   guest?                 *")
+        print("*             (q)    quit?                 *")
+        print("*                                          *")
+        print("********************************************")
+
+    def input_prompt(self):
+        while True:
+            self.menu_output()
+            command = input("Enter your command: ")
+            command = command.lower()
+            if command == "q":
+                print("you are quitting the program")
+                break
+            elif command == "1":
+                admin_menu()
+            elif command == "2":
+                pass
+            elif command == "3":
+                pass
+            else:
+                print("invalid input, please try again")
+
---
+++ RU Basement Open/ui/player_ui.py
@@ -0,0 +1,27 @@
+class Player_UI:
+    def __init__(self):
+        print("inside player_ui")
+
+    def player_menu1(self):
+            print("********************************************")
+            print("*               Hello Guest!               *")
+            print("*                                          *")
+            print("*       (1)  see teams and players?        *")
+            print("*       (2)  see upcoming matches?         *")
+            print("*       (3)  see completed matches?        *")
+            print("*       (4)  see table?                    *")
+            print("*       (5)  view player stats?            *")
+            print("*                                          *")
+            print("*       (b)  go back?                      *")
+            print("*       (q)  quit?                         *")
+            print("*                                          *")
+            print("********************************************")
+
+    
+    if 1 is pressed:
+        def player_menu1(self):
+            print("********************************************")
+            print("*                                          *")
+            print("*                                          *")
+            print("********************************************")
+
