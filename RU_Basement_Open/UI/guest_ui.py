5f92fee97c1b6bd5818fb902aaedeb7f797a0437
oskargreve <oskarg22@ru.is>
2022-11-30 12:07:37 +0000

    fært ui upplýsinhar í rétt skjal x2

---
+++ RU_Basement_Open/UI/guest_ui.py
@@ -0,0 +1,82 @@
+class Guest_UI:
+    def __init__(self):
+        print("inside player_ui")
+
+    def guest_menu(self):
+            print("********************************************")
+            print("*               Hello Guest!               *")
+            print("*                                          *")
+            print("*       (1)  see teams and players?        *")
+            print("*       (2)  see upcoming matches?         *")
+            print("*       (3)  see completed matches?        *")
+            print("*       (4)  see table?                    *")
+            print("*       (5)  view player stats?            *")
+            print("*                                          *")
+            print("*       (b)  go back?                      *")
+            print("*       (q)  quit?                         *")
+            print("*                                          *")
+            print("********************************************")
+
+    
+    if 1 is pressed:
+        def guest_menu1(self):
+            print("********************************************")
+            print("*            {team[players...]}            *")
+            print("*                                          *")
+            print("*{chelsea[siggi, jón, kolla, thunderbird]} *")
+            print("*{manchester[óskar, bjarni, ólafur, ingó]} *")
+            print("*{Liverpool[Erik, lína, sigríður, Binni]}  *")
+            print("*                                          *")
+            print("*       (b)  go back?                      *")
+            print("*       (q)  quit?                         *")
+            print("*                                          *")
+            print("********************************************")
+    
+
+    if 2 is pressed:
+        def guest_menu2(self):
+            print("********************************************")
+            print("*            Upcoming matches              *")
+            print("*                                          *")
+            print("*       {date} : {home} vs {away}          *")
+            print("*       {date} : {home} vs {away}          *")
+            print("*       {date} : {home} vs {away}          *")
+            print("*                                          *")
+            print("*       (b)  go back?                      *")
+            print("*       (q)  quit?                         *")
+            print("*                                          *")
+            print("********************************************")
+
+
+    if 3 is pressed:
+        def guest_menu3(self):
+            print("********************************************")
+            print("*           Completed matches              *")
+            print("*                                          *")
+            print("*     {home} {score} : {score} {away}      *")
+            print("*     {home} {score} : {score} {away}      *")
+            print("*     {home} {score} : {score} {away}      *")
+            print("*                                          *")
+            print("*       (b)  go back?                      *")
+            print("*       (q)  quit?                         *")
+            print("*                                          *")
+            print("********************************************")
+
+    
+    if 4 is pressed:
+        def guest_menu4(self):
+            print("********************************************")
+            print("*         Current table standings          *")
+            print("*                                          *")
+            print("*  Team       |  Points      |  Rounds w/l *")
+            print("* {team}      |  {points}    |  {w} - {l}  *")
+            print("* {team}      |  {points}    |  {w} - {l}  *")
+            print("* {team}      |  {points}    |  {w} - {l}  *")
+            print("*                                          *")
+            print("*        (b)  go back?                     *")
+            print("*        (q)  quit?                        *")
+            print("*                                          *")
+            print("********************************************")
+
+
+    #if 5 is pressed: þetta er B krafa
