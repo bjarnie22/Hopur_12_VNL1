99a202704419ef6f1f8cf7f5937aca5096397dbf
omt1 <olafur.magnus.torfason@gmail.com>
2022-12-04 22:29:11 +0000

    OT og Ingó ui update
    Now I have updated the case 8 (Create a tournament) and case and case 2 (View tournament) Ingó is working on case 9 (Start a tournament). Óskar has been working on the match ui and tomorrow we will finnish the ui layer.

--- RU_Basement_Open/UI/tournament_ui.py
+++ RU_Basement_Open/UI/tournament_ui.py
@@ -1,10 +1,13 @@
 class Tournament():
     def __init__(self):
         pass 
-    
-    # Here for c demand we can check if there is a ongoing tournament, then the user will get: 
-    # "Sorry there can only be one tournament going on at time"
+  
+
+############################# Create a tournament case number 8 in the wire frame###############################
+#################################################################################################################
+
     def create_a_tournament_menu(self):
+#Case number 8 in the wire frame 
         print("****************************************************")
         print("*                                                  *")
         print("*       (1)  Add a association                     *")
@@ -31,7 +34,7 @@ class Tournament():
 
 
     def add_association_menu(self):
-    # Here we need to send the info about how many teams are in that association    
+# Case number 11 in the wire frame 
         print("********************************************")
         print("*                                          *")
         print("*       (1)  Add team                      *")
@@ -58,15 +61,11 @@ class Tournament():
                 print("invalid input, please try again")
 
     def add_team_menu(self):
-    # Here we need to send the info about how many teams are in that association    
+# Case number 12 in the wire frame 
 
         print("********************************************")
         print("*                                          *")
         print("*       (1)  Add a player                  *")
-        print("* If you have added all the associations,  *")
-        print("* teams and players.                        *") 
-        print(" You should now schedule matches            *") 
-        print("*       (2)  Schedule matches              *")
         print("*       (b)  go back?                      *")
         print("*                                          *")
         print("********************************************")
@@ -91,7 +90,9 @@ class Tournament():
                 print("invalid input, please try again")
     
     def input_prompt_for_add_a_player (self):
+# Case number 13 in the wire frame 
         name_of_player = input("Name of the player: ")
+        email_of_the_player = input("Name of the player: ")
         social_security_number: input("Players social security number: ")
         date_of_birth = input("Date of birth: ")
         address_of_player = input("Home address of player: ") 
@@ -112,22 +113,17 @@ class Tournament():
             else:
                 print("invalid input, please try again")
     
-
-    def input_prompt_for_schedule_matches(self):
-        # Here we need to get a function that calculates the number of teams and number of rounds. And also 
-        # Here we would need to error check that all teams are playing equally. 
-        # Each match would need an id name? For the match class to function. 
-        pass
-
-########################################################################################################
+############################## View tournament case 2 in the wire frame#############################################
+# ###################################################################################################################
 
     def view_tournament_main_menu(self):
+# Case number 2 in the wire frame
             print("*************************************************************")
             print("*                   View tournament                         *")
             print("*                                                           *")
-            print("*       (1)  List of teams, their players info and stat     *")
+            print("*       (1)  List of associations and their teams.          *")
             print("*       (2)  List of upcoming and played matches            *")
-            print("*       (2)  List of current standing                       *")
+            print("*       (3)  List of current standing                       *")
             print("*       (b)  go back?                                       *")
             print("*                                                           *")
             print("*                                                           *")
@@ -147,14 +143,15 @@ class Tournament():
                 print("invalid input, please try again")
     
                 
-    def list_of_teams(self):
+    def list_of_associations_and_their_teams(self):
+# Case 3 in the wire frame 
             # Here we would need a list of teams, to which association they belong to 
             # and list of players. 
             pass
     
-    def input_prompt_for_list_of_teams(self):
+    def input_prompt_for_list_of_associations_and_their_teams(self):
         while True:
-            self.list_of_teams()
+            self.list_of_associations_and_their_teams()
             command = input("Enter the id of the team you would like to view and b if you would like to go back: ")
             command = command.lower()
             if command == "b":
@@ -164,7 +161,8 @@ class Tournament():
                 self.input_prompt_for_list_of_players(command)
                 # Here we need to check if the id is valid or not. 
 
-    def list_of_players(self):
+    def list_of_players_for_a_choose_team(self):
+# Case 4 in the wire frame 
             # Here we would need a list of players for a choosen team. 
             pass
     
@@ -183,135 +181,8 @@ class Tournament():
                 print("invalid input, please try again")
                 print("invalid input, please try again")
 
-    def a_single_player_info_and_stats(self):
-        # Here we would need public information of a player for a chosen team. 
-        pass
-    
-    
-    def input_prompt_for_a_single_player_and_info(self):
-        while True:
-            self.a_single_player_info_and_stats()
-            command = input("Enter any key to back: ")
-            command = command.lower()
-            if command:
-                break
 
 ########################################################################################################    
-    def input_prompt_for_main_menu(self):
 
-        while True:
-            self.tournament_main_menu()
-            command = input("Enter your command: ")
-            command = command.lower()
-            if command == "q":
-                print("you are quitting the program")
-                break
-            elif command == "1":
-                print("You pressed 1")
-                self.input_prompt_for_associations_menu()
-            elif command == "2":
-                print("You pressed 2")
-                view_tournament()
-
-                #captain_user.captain_menu1()
-            elif command == "3":
-                print("You pressed 2")
-            else:
-                print("invalid input, please try again")
-    
-    
-    def associations_menu(self): 
-            print("********************************************")
-            print("*              Associations                *")
-            print("*                                          *")
-            print("*       (1)  Add association               *")
-            print("*       (2)  View and or                   *")
-            print("*       update association                 *")
-            print("*       (b)  go back?                      *")
-            print("*                                          *")
-            print("********************************************")
-            input_prompt_for_associations_menu() 
-
-    def input_prompt_for_associations_menu(self):
-        add_association = Create_a_new_tournament()
-        while True:
-            self.menu_output()
-            command = input("Enter your command: ")
-            command = command.lower()
-            if command == "b":
-                print("you are going back")
-                break
-            elif command == "1":
-                add_association.input_prompt_for_add_association()
-            elif command == "2":
-                print("You pressed 2")
-                # view_association()
-
-                #captain_user.captain_menu1()
-            elif command == "3":
-                print("You pressed 2")
-            else:
-                print("invalid input, please try again")
-
-    
-
-    def create_a_teams():
-        pass
-
-    def create_a_player(): 
-        pass
-
-    def view_association():
-        pass
-
-    #Choose number of association to view: 
-    #Choose a number of association to update
-
-    def update_association(): 
-        pass
-
-    def view_tournament(): 
-        print("********************************************")
-        print("*               Hello!                     *")
-        print("*                                          *")
-        print("*       (1)  see teams and players?        *")
-        print("*       (2)  see upcoming matches?         *")
-        print("*       (3)  see completed matches?        *")
-        print("*       (4)  see table?                    *")
-        print("*       (5)  view player stats?            *")
-        print("*       (b)  go back?                      *")
-        print("*       (q)  quit?                         *")
-        print("*                                          *")
-        print("********************************************")
-
-    def input_prompt_for_view_tournament(self):
-
-        while True:
-            print ("im in the loop")
-            self.menu_output()
-            command = input("Enter your command: ")
-            command = command.lower()
-            if command == "q":
-                print("you are quitting the program")
-                break
-            elif command == "1":
-                print("You pressed 1")
-                # see teams and players() from logic layer  
-            elif command == "2":
-                print("You pressed 2")
-                # see upcoming matches? 
-                view_tournament()
-            elif command == "3":
-                print("You pressed 3")
-                # see_completed_matches() from logic layer
-                view_tournament()
-            elif command == "5":
-                print("You pressed 5")
-                # view player stats() from logic layer
-                view_tournament()
-            elif command == "6":
-                print("You pressed 5")
-                # Update score for a played game() from logic layer
-                view_tournament()
-            else:
-                print("invalid input, please try again")
+########################################################################################################       
+ 
