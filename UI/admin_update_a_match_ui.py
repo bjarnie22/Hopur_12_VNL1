027f7b08c213ece2525af62612562f53ce8ac9fa
richerdman <112634685+richerdman@users.noreply.github.com>
2022-12-05 17:06:53 +0000

    Add files via upload

---
+++ UI/admin_update_a_match_ui.py
@@ -0,0 +1,5 @@
+# This class needs to get an instance of match info, and if that is empty then, admin can not change it. if captain 
+# has updated it then admin can change it, and send it back to match info. 
+#  
+class admin_update_match(): 
+    
---
+++ UI/main_ui.py
@@ -0,0 +1,44 @@
+from UI.admin_ui import Admin_UI
+from UI.captain_ui import Captain_UI
+from UI.guest_ui import Guest_UI
+
+class Main_UI:
+    def __init__(self):
+        print("inside main_UI")
+
+    def menu_output(self):
+        '''starting menu'''
+        print("********************************************")
+        print("*                   Hello!                 *")
+        print("*                                          *")
+        print("*             (1)   admin?                 *")
+        print("*             (2) captain?                 *")
+        print("*             (3)   guest?                 *")
+        print("*             (q)    quit?                 *")
+        print("*                                          *")
+        print("********************************************")
+
+
+    def input_prompt(self):
+        while True:
+            print ("im in the loop")
+            self.menu_output()
+            command = input("Enter your command: ")
+            command = command.lower()
+            if command == "q":
+                print("you are quitting the program")
+                break
+            elif command == "1":
+                admin_user = Admin_UI()
+                print("You pressed 1")
+                admin_user.input_prompt_for_admin_menu()
+            elif command == "2":
+                captain_user = Captain_UI()
+                print("You pressed 2")
+                captain_user.input_prompt()
+            elif command == "3":
+                guest_user = Guest_UI()
+                print("You pressed 3")
+                guest_user.input_prompt()
+            else:
+                print("invalid input, try again!")
